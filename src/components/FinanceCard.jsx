d2ad7045e48e5fc67b5725311160244928bd27d3
George <demina.ivann@gmail.com>
2025-02-14 11:15:37 -0800

    Make locale "default"

--- src/components/FinanceCard.jsx
+++ src/components/FinanceCard.jsx
@@ -7,7 +7,7 @@ export default function FinanceCard({
   editRecord,
   deleteRecord,
 }) {
-  const date = new Intl.DateTimeFormat("en-US", {
+  const date = new Intl.DateTimeFormat("default", {
     day: "2-digit",
     month: "2-digit",
     year: "2-digit",
